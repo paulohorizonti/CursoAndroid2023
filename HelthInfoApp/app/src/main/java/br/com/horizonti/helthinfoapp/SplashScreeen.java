ee369f773f5c2cce38980f6fee8dbb971d075f20
paulohorizonti <pnagi2012@gmail.com>
2023-03-08 21:06:57 -0300

    Commit continuarndo o projeto

---
+++ HelthInfoApp/app/src/main/java/br/com/horizonti/helthinfoapp/SplashScreeen.java
@@ -0,0 +1,14 @@
+package br.com.horizonti.helthinfoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreeen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screeen);
+    }
+}
