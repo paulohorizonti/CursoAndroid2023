14e7f118b8b6df61333e742260c3920774859cd4
paulohorizonti <pnagi2012@gmail.com>
2023-03-14 22:30:42 -0300

    COMMIT ATE AULA 28

---
+++ AppListaCursos/app/src/main/java/br/com/devandroid/paulo/applistacursos/model/Pessoa.java
@@ -0,0 +1,47 @@
+package br.com.devandroid.paulo.applistacursos.model;
+
+public class Pessoa {
+    //Atributos e metodos
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneDeContato;
+
+    //Contrutor
+    public Pessoa(){}
+
+    //Metodos de Acesso aos atributos
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneDeContato() {
+        return telefoneDeContato;
+    }
+
+    public void setTelefoneDeContato(String telefoneDeContato) {
+        this.telefoneDeContato = telefoneDeContato;
+    }
+}
--- AppListaCursos/app/src/main/java/br/com/devandroid/paulo/applistacursos/view/MainActivity.java
+++ AppListaCursos/app/src/main/java/br/com/devandroid/paulo/applistacursos/view/MainActivity.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.devandroid.paulo.applistacursos.R;
+import br.com.devandroid.paulo.applistacursos.model.Pessoa;
+
 //classe que vai instaciar o xml do layout
 public class MainActivity extends AppCompatActivity {
+    //objeto pessoa
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //instanciando o objto
+        pessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome("PAULO ROBERTO");
+        pessoa.setSobreNome("NOGUEIRA");
+        pessoa.setCursoDesejado("ANDRODID");
+        pessoa.setTelefoneDeContato("63-992062543");
     }
 }
