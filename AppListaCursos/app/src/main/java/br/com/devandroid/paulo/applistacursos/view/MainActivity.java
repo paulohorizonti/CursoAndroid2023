d0a2bf57f9fdb405195503cd79bede1ff716e8e8
paulohorizonti <pnagi2012@gmail.com>
2023-03-16 00:40:09 -0300

    COMMIT - AULA 34 - BOTOES

--- AppListaCursos/app/src/main/java/br/com/devandroid/paulo/applistacursos/view/MainActivity.java
+++ AppListaCursos/app/src/main/java/br/com/devandroid/paulo/applistacursos/view/MainActivity.java
@@ -3,6 +3,11 @@ package br.com.devandroid.paulo.applistacursos.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.devandroid.paulo.applistacursos.R;
 import br.com.devandroid.paulo.applistacursos.model.Pessoa;
@@ -13,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
 
     String dadosPessoa;
+
+    //componentes do layout, tendo o mesmo ID que esta no xml
+    EditText edtNome;
+    EditText edtSobrenome;
+    EditText edtCurso;
+    EditText edtTelefone;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +46,56 @@ public class MainActivity extends AppCompatActivity {
         dadosPessoa += pessoa.getPrimeiroNome();
         dadosPessoa = "Sobrenome: ";
         dadosPessoa += pessoa.getSobreNome();
+
+        edtNome = findViewById(R.id.edtNome);
+        edtSobrenome = findViewById(R.id.edtSobrenome);
+        edtTelefone = findViewById(R.id.edtTelefone);
+        edtCurso = findViewById(R.id.edtCurso);
+
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        edtNome.setText(pessoa.getPrimeiroNome());
+        edtSobrenome.setText(pessoa.getSobreNome());
+        edtTelefone.setText(pessoa.getTelefoneDeContato());
+        edtCurso.setText(pessoa.getCursoDesejado());
+
+
+        //metodo do botao
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                edtNome.setText("");
+                edtSobrenome.setText("");
+                edtTelefone.setText("");
+                edtCurso.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre !", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(edtNome.getText().toString());
+                pessoa.setSobreNome(edtSobrenome.getText().toString());
+                pessoa.setTelefoneDeContato(edtTelefone.getText().toString());
+                pessoa.setCursoDesejado(edtCurso.getText().toString());
+
+
+                Toast.makeText(MainActivity.this, "Salvo : "+pessoa.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+        Log.i("POOAndroid", pessoa.toString());
+
     }
 }
